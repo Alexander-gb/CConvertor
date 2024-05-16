e952d34d66188bb2f947aeb5f0b828b09f0bd21f
Alexander <frosti-sizov@rambler.ru>
2024-05-16 12:30:51 +0400

    added logic

--- src/main/java/com/example/currencyconvertor/controller/CurrencyController.java
+++ src/main/java/com/example/currencyconvertor/controller/CurrencyController.java
@@ -1,6 +1,5 @@
 package com.example.currencyconvertor.controller;
 
-
 import com.example.currencyconvertor.model.Currency;
 import com.example.currencyconvertor.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
+import javax.money.UnknownCurrencyException;
+import javax.money.format.MonetaryAmountFormat;
+import javax.money.format.MonetaryFormats;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class CurrencyController {
@@ -20,12 +26,23 @@ public class CurrencyController {
 
     @GetMapping("/convert/{from}/to/{to}")
     @ResponseBody
-    public BigDecimal convert(@PathVariable String from, @PathVariable String to,
-                              @RequestParam BigDecimal amount, @RequestParam LocalDate date) {
-        Currency fromCurrency = currencyService.findByCode(from);
-        Currency toCurrency = currencyService.findByCode(to);
-        BigDecimal result = currencyService.convert(fromCurrency, toCurrency, amount, date);
-        return result;
+    public ResponseEntity<String> convert(@PathVariable String from, @PathVariable String to,
+                                          @RequestParam BigDecimal amount, @RequestParam LocalDate date) {
+        try {
+            Currency fromCurrency = currencyService.findByCode(from);
+            Currency toCurrency = currencyService.findByCode(to);
+            BigDecimal result = currencyService.convert(fromCurrency, toCurrency, amount, date);
+
+            MonetaryAmountFormat format = MonetaryFormats.getAmountFormat(Locale.getDefault());
+            MonetaryAmount resultAmount = Monetary.getDefaultAmountFactory().setCurrency(toCurrency.getCurrencyUnit()).setNumber(result).create();
+            String formattedResult = format.format(resultAmount);
+
+            return new ResponseEntity<>(formattedResult, HttpStatus.OK);
+        } catch (UnknownCurrencyException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred while processing the request.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/currencies")
@@ -40,5 +57,4 @@ public class CurrencyController {
         BigDecimal rate = currency.getConversionFactor();
         return new ResponseEntity<>(rate, HttpStatus.OK);
     }
-
 }
--- src/main/java/com/example/currencyconvertor/model/Currency.java
+++ src/main/java/com/example/currencyconvertor/model/Currency.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.javamoney.moneta.Money;
 
+import javax.money.Monetary;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -13,17 +15,65 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @Entity
-
-
 public class Currency {
     @Id
-    String name;
-    String code;
-    BigDecimal quantity;
-    BigDecimal conversionFactor;
-    LocalDate date;
+    private String name;
+    private String code;
+    private BigDecimal quantity;
+    private BigDecimal conversionFactor;
+    private LocalDate date;
+    private String currencyUnit;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+        this.currencyUnit = String.valueOf(Monetary.getCurrency(code));
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
 
     public BigDecimal getConversionFactor() {
         return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCurrencyUnit() {
+        return currencyUnit;
+    }
+
+    public void setCurrencyUnit(String currencyUnit) {
+        this.currencyUnit = currencyUnit;
+    }
+
+    public Money getAmount() {
+        return Money.of(getQuantity(), getCurrencyUnit());
     }
 }
